998764e58b518f6d46b2767bc07bd0ef7f55681a
MeghaShivhare <meghashivhare8@gmail.com>
2026-01-15 11:26:48 +0530

    add user script .sh

--- 02-Tf-Settings-Providers-Resources/terraform-manifests/c2-ec2instance.tf
+++ 02-Tf-Settings-Providers-Resources/terraform-manifests/c2-ec2instance.tf
@@ -1,4 +1,11 @@
 resource "aws_instance" "myec2" { # lables aws_instance - resource type and ec2demo - resource local name
   ami           = "ami-068c0051b15cdb816" # resource-arguments
   instance_type = "t2.micro" 
+  user_data = file("${path.module}/app-install.sh")
+  tags = {
+    "Name" = "EC2 Demo"
+  }
+
+  # Argument Reference - what you provide as input - ex ami-id
+  # Attribute Reference  are output for ex instance id that you will get in return after execution
 }
